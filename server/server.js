4d2d8a9275a578b2a4727b487d918834ad8fcffe
sadrilamin1999 <sadrilshuvo420@gmail.com>
2024-03-28 02:20:56 +0600

    router updated

--- server/server.js
+++ server/server.js
@@ -1,15 +1,40 @@
 const http = require("http");
+const fs = require("fs");
+
+//server create
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("This is Home page");
-    res.end();
-  } else if (req.url === "/about") {
-    res.write("This is About Page");
-    res.end();
-  } else {
-    res.write("Not found!");
-    res.end();
+  //set header
+  res.setHeader("Content-Type", "text/html");
+
+  //routes
+  let path = "./html/";
+  switch (req.url) {
+    case "/":
+      path += "/index.html";
+      res.statusCode = 200;
+      break;
+
+    case "/about":
+      path += "/about.html";
+      res.statusCode = 200;
+      break;
+
+    default:
+      path += "/not-found.html";
+      res.statusCode = 404;
+      break;
   }
+  //send response to html client
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
-server.listen(3000);
-console.log("Listening on port 3000");
+server.listen(8000, "localhost", () => {
+  console.log("Server running on port 8000");
+});
